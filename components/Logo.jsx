732f66dc0fa704046c816943aebe1d675a224b79
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-11-12 10:58:44 -0400

    Logo translations for "es" and "en"

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,5 +1,7 @@
 import Col from "react-bootstrap/Col"
+import { useTranslation, Trans } from 'next-i18next'
 function Logo() {
+    const { t } = useTranslation('login')
     return <Col md={6} className="text-center">
         <h1 className="text-white">
             YuYu
