a5b83c5b97a4468ef94ac4e95ba2ecbb70958c88
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-11-12 11:02:18 -0400

    Add logo locale

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,7 +1,7 @@
 import Col from "react-bootstrap/Col"
 import { useTranslation, Trans } from 'next-i18next'
 function Logo() {
-    const { t } = useTranslation('login')
+    const { t } = useTranslation('logo')
     return <Col md={6} className="text-center">
         <h1 className="text-white">
             YuYu
@@ -9,6 +9,9 @@ function Logo() {
         <h6 className="text-white fw-bold fs-5">
             Aprende ingl&eacute;s del d&iacute;a a d&iacute;a sin estudiar gram&aacute;tica.
         </h6>
+        <Trans i18nKey='logo'>
+            Then you may have a look at <a href={t('blog.optimized.link')}>this blog post</a>.
+        </Trans>
     </Col>
 }
 export default Logo
