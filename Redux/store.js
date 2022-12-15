4d60d733f226e75458becc249b265120735fc328
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-12-14 22:15:28 -0400

    Remove redux folder

--- Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import langsDucks from "./langsDucks";
-export const store = configureStore({
-  reducer: {
-    langsData: langsDucks,
-  },
-  middleware: [thunk],
-});
