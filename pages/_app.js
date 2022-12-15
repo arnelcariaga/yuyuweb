177b98b13060540d3286f3252f6ebfc4b9293fae
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-12-14 22:15:03 -0400

    Remove redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import { store } from "../Redux/store";
 import { appWithTranslation } from "next-i18next";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
