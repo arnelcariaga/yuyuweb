b4ad0d2d18428408d69eb6eb99519c3ead452db6
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-12-14 22:20:30 -0400

    Add redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "../Redux/store";
 import { appWithTranslation } from "next-i18next";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </SessionProvider>
   );
 }
