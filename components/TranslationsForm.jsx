287fb029cb75a3e2907e2f0ff830819d9bdd8f3f
Arnel Cariaga <arnelcariaga@hotmail.com>
2022-12-14 22:13:31 -0400

    Select input for category and his transalations

--- components/TranslationsForm.jsx
+++ components/TranslationsForm.jsx
@@ -44,7 +44,8 @@ function TranslationsForm() {
     const { t } = useTranslation("common");
     let translationsFormDescription = t("translationsFormDescription"),
         numberOfPhraseLabel = t("numberOfPhraseLabel"),
-        saveTransalationBtn = t("saveTransalationBtn");
+        saveTransalationBtn = t("saveTransalationBtn"),
+        selectCategoryLabel = t("selectCategoryLabel");
 
     const addPhraseFunc = () => {
         let newArray = [];
@@ -105,6 +106,23 @@ function TranslationsForm() {
                             </IconContext.Provider>
                         </Button>
 
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={4} className="mb-3">
+                        <Form.Select
+                            className="w-auto"
+                            aria-label={selectCategoryLabel}
+                            isInvalid={errors.category}
+                            {...register("category", {
+                                required: true
+                            })}
+                        >
+                            <option value="">{selectCategoryLabel}</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
                     </Col>
                 </Row>
                 {
