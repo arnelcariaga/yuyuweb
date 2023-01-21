bf1730e75cb16fcba1c34dddc4eb779744bc00df
Arnel Cariaga <arnelcariaga@hotmail.com>
2023-01-21 12:16:28 -0400

    Upload audios only for example and edit translations table

--- components/TranslationsDataTable.jsx
+++ components/TranslationsDataTable.jsx
@@ -3,50 +3,115 @@ import DataTable from 'react-data-table-component';
 import { useSelector, useDispatch } from "react-redux";
 import { getTranslationsAction } from "./../Redux/translationsDuck";
 import Button from 'react-bootstrap/Button';
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa"
 
-const columns = [
-    {
-        name: 'Frase en ingles',
-        cell: (row, index, column, id) => {
-            var audio = new Audio(row.englishPhraseAudio);
-            console.log(audio.ENTITY_REFERENCE_NODE)
-            return <div>
-                {row.englishPhrase}
-                <br />
-                <Button variant="success" size="sm" type="button" onClick={() => {
-                    if (audio.paused) {
-                        audio.play();
-                    } else {
-                        audio.pause();
-                    }
-                }}>
-                    PLya
-                </Button>
-            </div>
+const audioInstance = (audio) => {
+    return new Audio(audio);
+}
+
+const Cell = ({ row }) => {
+    const [newData, setNewData] = React.useState([{
+        ...row,
+        isPlaying: false
+    }])
+
+    let audio = new Audio(newData.englishPhraseAudio);
+
+    const playFunction = (id) => {
+        if (audio.paused) {
+            audio.play();
+            const newItem = newData.find(item => item._id === id).isPlaying = !row.isPlaying
+            console.log("newItem", newItem)
+            //setNewData(newItem)
+        } else {
+            audio.pause();
+            const newItem = newData.find(item => item._id === id).isPlaying = !row.isPlaying
         }
-    },
-    {
-        name: 'Traducido a tu idioma',
-        cell: (row, index, column, id) => {
-            return <div>
-                {row.howToSay}
-                {row.howToSayAudio}
-            </div>
-        }
-    },
-];
+    }
+
+    console.log(newData)
+
+    return <div className="d-flex align-items-center" data-tag="allowRowEvents">
+        {row.englishPhrase}
+        <Button
+            variant="default"
+            type="button"
+            className="rounded-circle p-0 ms-2"
+            data-tag="allowRowEvents"
+            onClick={() => playFunction(row._id)}>
+            klk
+            {
+                //newRowData.isPlaying ? <FaPauseCircle /> : <FaPlayCircle />
+            }
+        </Button>
+    </div>
+}
+
+const Columns = (t) => {
+    let phrase = t("phrase"),
+        phraseTranslatedOnYourLang = t("phraseTranslatedOnYourLang");
+
+    const data = [
+        {
+            name: phrase,
+            sortable: true,
+            cell: (row) => <Cell row={row} />
+        },
+        {
+            name: phraseTranslatedOnYourLang,
+            sortable: true,
+            cell: (row, index, column, id) => {
+                var audio = new Audio(row.howToSayAudio);
+                return <div className="d-flex align-items-center">
+                    {row.englishPhrase}
+                    <Button
+                        variant="default"
+                        type="button"
+                        className="rounded-circle p-0 ms-2"
+                        onClick={() => {
+                            if (audio.paused) {
+                                audio.play();
+                            } else {
+                                audio.pause();
+                            }
+                        }}>
+                        <FaPlayCircle />
+                    </Button>
+                </div>
+            }
+        },
+    ];
+
+    return data
+}
 
 function TranslationsDataTable() {
     const dispatch = useDispatch();
     const translations = useSelector((s) => s.translationsData.translations);
+    const { t } = useTranslation("common");
+    let addTranslations = t("addTranslations"),
+        noDataTableFound = t("noDataTableFound");
+
+    const [audios, setAudios] = React.useState([])
 
     React.useEffect(() => {
         dispatch(getTranslationsAction());
     }, [dispatch]);
 
-    return <DataTable
-        columns={columns}
-        data={translations}
-    />
+    return <>
+        <Link href="/dashboard">
+            <Button size="sm" type="button" className="float-end mb-2">
+                {addTranslations}
+            </Button>
+        </Link>
+
+        <DataTable
+            columns={Columns(t)}
+            data={translations}
+            noDataComponent={noDataTableFound}
+        />
+    </>
 }
 export default TranslationsDataTable
