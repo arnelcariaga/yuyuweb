67687c04def5317cb085f9797af249481724026e
Arnel Cariaga <arnelcariaga@hotmail.com>
2023-01-25 13:46:41 -0400

    Add userType in databse model

--- utils/models/user.model.jsx
+++ utils/models/user.model.jsx
@@ -15,6 +15,10 @@ const User = new mongoose.Schema(
       type: String,
       required: true,
       unique: true
+    },
+    userType: {
+      type: Number,
+      required: true
     }
   },
   {
