8da1edc827ac58910241cfd85c58fb02a1144e49
Arnel Cariaga <arnelcariaga@hotmail.com>
2023-01-25 13:43:20 -0400

    Add translations.js file to get info by user

---
+++ pages/api/translations.js
@@ -0,0 +1,39 @@
+import connectDB from "./../../middleware/mongoDB";
+const Translation = require("./../../utils/models/translations.model");
+import { getSession } from "next-auth/react";
+
+async function handler(req, res) {
+  connectDB();
+
+  if (req.method === "GET") {
+    // Process a POST request
+    try {
+      const session = await getSession(res);
+      const userId = session.user.id,
+        userType = session.user.userType;
+
+      const translations =
+        userType === 2
+          ? await Translation.find({}).sort({ createdAt: -1 })
+          : await Translation.find({ "addedBy.userId": userId }).sort({
+              createdAt: -1,
+            });
+
+      res.json({
+        status: "ok",
+        error: "nada",
+        data: translations,
+      });
+    } catch (err) {
+      res.json({
+        status: "error",
+        error: "server error",
+        data: err,
+      });
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
+
+export default handler;
